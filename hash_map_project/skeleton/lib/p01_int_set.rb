bdb8ae49a77d4344a2f6076e5c188adeaa624565
Jenny Nhan <j3nnynhan@gmail.com>
2021-06-07 15:46:04 -0700

    finished int set

--- hash_map_project/skeleton/lib/p01_int_set.rb
+++ hash_map_project/skeleton/lib/p01_int_set.rb
@@ -7,8 +7,8 @@ class MaxIntSet
   end
 
   def insert(num)
-    store[num] = true
     raise 'Out of bounds' if num > max || num < 0
+    store[num] = true
   end
 
   def remove(num)
@@ -58,29 +58,59 @@ class IntSet
   end
 end
 
+
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :prev_num_buckets
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { [] }
+    @prev_num_buckets = num_buckets / 2
     @count = 0
   end
 
-  def insert(num); end
+  def insert(num)
+    unless include?(num)
+      self[num].push(num) 
+      @count += 1
+    end
 
-  def remove(num); end
+    if count > num_buckets
+      resize!
+    end
+  end
 
-  def include?(num); end
+  def remove(num)
+    if include?(num)
+      self[num].delete(num) 
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
     @store.length
   end
 
-  def resize!; end
+  def resize!
+    prev_store = @store
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+
+    prev_store.each do |bucket|
+      bucket.each do |ele|
+        insert(ele)
+      end
+    end
+
+  end
 end
