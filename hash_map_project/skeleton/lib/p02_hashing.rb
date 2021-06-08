c5cd61600898c438eece83b9b000c81f576e44db
Joseph Kim <jkjoeeekim@live.com>
2021-06-07 17:41:11 -0700

    Finish hashing

--- hash_map_project/skeleton/lib/p02_hashing.rb
+++ hash_map_project/skeleton/lib/p02_hashing.rb
@@ -11,7 +11,9 @@ end
 
 class String
   def hash
-    self.chars
+    alpha = [*'a'..'z', *'A'..'Z', *'0'..'9']
+    code = self.chars.map! { |char| alpha.index(char) }
+    code.join.to_i.hash
   end
 end
 
@@ -19,6 +21,16 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    code = []
+    alpha = [*'a'..'z', *'A'..'Z', *'0'..'9']
+    sorted_keys = keys.sort
+    sorted_values = values.sort
+    sorted_keys.each do |key|
+      code << [key].map { |ele| alpha.index(ele) }.join
+    end
+    sorted_values.each do |value|
+      code << [value].map { |ele| alpha.index(ele) }.join
+    end
+    code.hash
   end
 end
